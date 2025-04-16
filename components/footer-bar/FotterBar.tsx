ee3b139487829515c1b499d3f048ee818a89b54b
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 15:25:02 +0530

    Add FotterBar component for footer content display

---
+++ components/footer-bar/FotterBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FotterBar = () => {
+  return (
+    <div className="conatiner max-auto py-6">
+        <p className="text-center">
+        Released under the MIT License.
+<br/>
+Copyright © 2022-present Yusuke Wada & Hono contributors. "kawaii" logo is created by SAWARATSUKI.
+        </p>
+    
+    </div>
+  )
+}
+
+export default FotterBar
