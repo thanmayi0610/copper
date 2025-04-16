150c7d0ef6294acf0bcc0d59a33e1665a867ea8f
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 15:33:27 +0530

    Fix FooterBar import and adjust layout in Rootpage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import NavigationBar from '@/components/navigation-bar/NavigationBar'
+import FooterBar from '@/components/footer-bar/FotterBar'
 import React from 'react'
 
 const Rootpage = () => {
   return (
     <div className="flex flex-col items-stretch">
       <NavigationBar/>
+       
       <div className="h-48 bg-orange-500"/>
       <div className="h-48 bg-purple-500"/>
       <div className="h-28 bg-green-500"/>
-      
+      <FooterBar/> 
     </div>
   )
 }
