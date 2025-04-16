4e1b1bf26d9911c272ed31b396218861455e43b8
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 15:24:45 +0530

    Add NavigationBar component to Rootpage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import NavigationBar from '@/components/navigation-bar/NavigationBar'
 import React from 'react'
 
 const Rootpage = () => {
   return (
     <div className="flex flex-col items-stretch">
-      <div className="h-28 bg-blue-500"/>
+      <NavigationBar/>
       <div className="h-48 bg-orange-500"/>
       <div className="h-48 bg-purple-500"/>
       <div className="h-28 bg-green-500"/>
