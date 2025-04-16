07291d736a4a16ddd34a73534299457821137a2c
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 14:45:24 +0530

    Add Rootlayout component to manage application layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
 
 import "./globals.css";
 
+import React, { PropsWithChildren } from 'react'
+
+const Rootlayout = (props: PropsWithChildren) => {
+  return (
+    <html lang="en">
+      <body>
+        {props.children}
+      </body>
+
+    </html>
+  );
+};
+
+export default Rootlayout
