2ac86f063118c58fe0174210dd18034fe6223d5c
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 15:33:32 +0530

    Refactor FotterBar component structure and fix container class name

--- components/footer-bar/FotterBar.tsx
+++ components/footer-bar/FotterBar.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 const FotterBar = () => {
   return (
-    <div className="conatiner max-auto py-6">
-        <p className="text-center">
+
+    <footer className="footer sm:footer-horizontal footer-center text-slate-400 text-lg p-4">
+    <aside>
+    <p className="text-center">
         Released under the MIT License.
 <br/>
 Copyright © 2022-present Yusuke Wada & Hono contributors. "kawaii" logo is created by SAWARATSUKI.
         </p>
-    
-    </div>
+    </aside>
+  </footer>
   )
 }
 
