6a95c2c05006b95990f07814620a0f5da7bd8d29
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-16 15:00:52 +0530

    Restore content in Rootpage component with layout structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Rootpage = () => {
   return (
-    <></>
+    <div className="flex flex-col items-stretch">
+      <div className="h-28 bg-blue-500"/>
+      <div className="h-48 bg-orange-500"/>
+      <div className="h-48 bg-purple-500"/>
+      <div className="h-28 bg-green-500"/>
+      
+    </div>
   )
 }
 
