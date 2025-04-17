ce0ee6ee6509052e1548aedcf7b43d9969684003
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-17 10:28:02 +0530

    Fix component imports and update RootPage structure for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
-import NavigationBar from '@/components/navigation-bar/NavigationBar'
-import FooterBar from '@/components/footer-bar/FotterBar'
-import React from 'react'
 
-const Rootpage = () => {
+
+import FooterBar from "@/components/footer-bar/FotterBar";
+import NavigationBar from "@/components/navigation-bar/NavigationBar";
+import React from "react";
+import HeroBlock from "./blocks/hero-block/HeroBlock";
+import FeaturesBlock from "./blocks/features-block/FeatureBlock";
+
+const RootPage = () => {
   return (
     <div className="flex flex-col items-stretch">
-      <NavigationBar/>
-       
-      <div className="h-48 bg-orange-500"/>
-      <div className="h-48 bg-purple-500"/>
-      <div className="h-28 bg-green-500"/>
-      <FooterBar/> 
+      <NavigationBar />
+      <div className="container mx-auto p-24">
+        <HeroBlock />
+      </div>
+      <div className="container mx-auto p-24">
+        <FeaturesBlock />
+      </div>
+      <FooterBar />
     </div>
-  )
-}
+  );
+};
 
-export default Rootpage
+export default RootPage;
