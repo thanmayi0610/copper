77a0f0700c1868d451e05bd5c06c72b6557bd331
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-17 10:28:26 +0530

    Add FeaturesBlock component to display key features with emojis and descriptions

---
+++ app/blocks/features-block/FeatureBlock.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const features = [
+  {
+    emoji: "🚀",
+    title: "Ultrafast & Lightweight",
+    description:
+      "The router RegExpRouter is really fast. The hono/tiny preset is under 14kB. Using only Web Standard APIs.",
+  },
+  {
+    emoji: "🌍",
+    title: "Multi-runtime",
+    description: "Works on Cloudflare, Fastly, Deno, Bun, AWS, or Node.js. The same code runs on all platforms.",
+  },
+  {
+    emoji: "🔋",
+    title: "Batteries Included",
+    description:
+      "Hono has built-in middleware, custom middleware, third-party middleware, and helpers. Batteries included.",
+  },
+  {
+    emoji: "😊",
+    title: "Delightful DX",
+    description: 'Super clean APIs. First-class TypeScript support. Now, we\'ve got "Types".',
+  },
+];
+
+const FeaturesBlock = () => {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      {features.map((feature) => {
+        return (
+          <div key={feature.title} className="card card-border bg-base-200 p-8">
+            <div className="flex flex-col gap-2 items-start">
+              <div className="text-2xl py-2 px-3 bg-base-100 rounded-md">{feature.emoji}</div>
+              <h2 className="card-title text-xl font-bold">{feature.title}</h2>
+              <p className="text-lg">{feature.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FeaturesBlock;
+  
+
